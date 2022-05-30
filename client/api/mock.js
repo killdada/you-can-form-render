b9f0a2f1a184f31f041c99656a85e1074a062cd6
yen <1124268308@qq.com>
2022-05-30 16:10:01 +0800

    fix: 输出错误

--- client/api/mock.js
+++ client/api/mock.js
@@ -3,19 +3,21 @@ const { getMockData, matchMock } = require('../mock');
 
 module.exports = (req, res) => {
   console.log('mock.js', req.body, req);
-  try {
-    const { mockData } = getMockData({
-      cwd: path.join(__dirname, '../'),
-    });
-    const match = mockData && matchMock(req, mockData);
+  // try {
+  const { mockData } = getMockData({
+    cwd: path.join(__dirname, '../'),
+  });
+  const match = mockData && matchMock(req, mockData);
 
-    if (match) {
-      console.log(`mock matched: [${match.method}] ${match.path}`);
-      res.send(match.handler(req, res));
-    } else {
-      res.send('api接口不存在');
-    }
-  } catch (e) {
-    console.error('mock.js', e, req.body);
+  window.test.ii();
+
+  if (match) {
+    console.log(`mock matched: [${match.method}] ${match.path}`);
+    res.send(match.handler(req, res));
+  } else {
+    res.send('api接口不存在');
   }
+  // } catch (e) {
+  //   console.error('mock.js', e, req.body);
+  // }
 };
